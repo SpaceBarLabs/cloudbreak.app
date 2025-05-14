83788ec26b0f3b56e38c87248bb293f31fd4f530
Ben Oakes + Zachary Morek <engineering+boakes+zmorek@spacebarlabs.com>
2025-05-14 18:37:06 +0000

    checkpoint

---
+++ TODO.md
@@ -0,0 +1,1 @@
+* configure dev.cloudbreak.app ?
---
+++ dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run Jekyll with development config
+bundle exec jekyll serve --config _config.yml,_config_development.yml --host 0.0.0.0 --port 4000
